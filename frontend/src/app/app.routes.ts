5ae32727af88dc08169fe818bc5a86a2a2594adb
git-devtest <git@github.com>
2026-06-11 17:04:35 -0500

    chore(frontend): add lazy loaded routes

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'certificar',
+    pathMatch: 'full'
+  },
+  {
+    path: 'certificar',
+    loadComponent: () =>
+      import('./components/certificar/certificar').then(m => m.CertificarComponent)
+  },
+  {
+    path: 'verificar',
+    loadComponent: () =>
+      import('./components/verificar/verificar').then(m => m.VerificarComponent)
+  },
+  {
+    path: 'historial',
+    loadComponent: () =>
+      import('./components/historial/historial').then(m => m.HistorialComponent)
+  }
+];
