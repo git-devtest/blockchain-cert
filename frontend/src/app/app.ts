d5f642d9499272205b951b3550f985d85524e66c
git-devtest <git@github.com>
2026-09-04 11:01:52 -0500

    feat(frontend): inject auth service in root component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from './services/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected title = 'Blockchain Cert';
+
+  constructor(public authService: AuthService) {}
 }
